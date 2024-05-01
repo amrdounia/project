dec9d753d8d0264355d9de9f098f4e485c11ceef
dounia <anrdounia@gmail.com>
2024-05-01 09:36:31 +0100

    newww

--- models/users.js
+++ models/users.js
@@ -3,17 +3,9 @@ const Schema = mongoose.Schema;
 
 // Schéma pour les utilisateurs
 const userSchema = new Schema({
-    FirstName:{ type: String, required: true ,minlength: 3}, 
-    LastName:{ type: String, required: true,minlength: 3},
-    CarType:{ type: String},
-    CarId:{ type: String},
-    PhoneNumber:{ type: String, required: true},
+    FullName:{ type: String, required: true ,minlength: 3}, 
     email:{ type: String, required: true, unique: true},
     password:{ type: String, required: true ,minlength: 8}, 
-    cars: [{latitude: {type: Number,required: true },
-      longitude: {type: Number,required: true},
-      timestamp: {type: Date,default: Date.now}
-      }]
 })
 
 // Modèle pour les utilisateurs
